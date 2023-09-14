26b170f883ef6de41e0a00c2ea737adf63693266
Erin Cathignol <erin.cathignol@recia.fr>
2023-09-14 15:48:16 +0200

    avancement addUser.ps1

--- addUser.ps1
+++ addUser.ps1
@@ -1,6 +1,19 @@
 $PATH= "userList.txt"
-$USER= Get-Content $PATH
+$DATA= cat $PATH
+$FLAG= "none"
 
-Write-Output $USER
+foreach ($USER in $DATA)
+{
+    if ($USER -like "## PERSONNEL*")
+    {
+        $FLAG= "personnel"
+    }
+    elseif ($User -like "## ELEVES*") {
+        $FLAG="eleve"
+    }
+    echo $FLAG >> userList.txt
+}
+
+
 
 # New-ADUser -Name "Jean DUPONT" -GivenName "Jean" -Surname "DUPONT" -SamAccountName "jdupont" -UserPrincipalName "jdupont@lab.intra" -AccountPassword (ConvertTo-SecureString -AsPlainText "Pàssw0rd" -Force) -Enable $true -ChangePasswordAtLogon $true -Path "OU=Users,OU=IT,OU=Services,DC=LAB,DC=INTRA"
