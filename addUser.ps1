e948785188b83ef8d9e32b61e4d937d5b2ca04af
Erin Cathignol <erin.cathignol@recia.fr>
2023-09-15 10:06:07 +0200

    avancement addUser.ps1 + linting userList.txt

--- addUser.ps1
+++ addUser.ps1
@@ -1,17 +1,46 @@
-$PATH= "userList.txt"
-$DATA= cat $PATH
-$FLAG= "none"
+##### INITIALISATION #####
 
-foreach ($USER in $DATA)
+$path= "userList.txt"
+$data= cat $path
+$flag= "none"
+$name= "none"
+$givenNameFirstLetter= "none"
+$givenNameLastLetters= "none"
+$givenName= "none"
+$surname= "none"
+$samAccountName= "none"
+$domain= "none"
+$userPrincipalName= "none"
+$accountPassword= "none"
+
+######### SCRIPT #########
+
+foreach ($user in $data)
 {
-    if ($USER -like "## PERSONNEL*")
-    {
-        $FLAG= "personnel"
+    if ($user -like "## PERSONNEL*") {
+        $flag= "personnel"
     }
-    elseif ($User -like "## ELEVES*") {
-        $FLAG="eleve"
+    elseif ($user -like "## ELEVES*") {
+        $flag="eleve"
     }
-    echo $FLAG >> userList.txt
+    else {
+        if ($flag -eq "personnel") {
+            $givenName= $user.split(" ")[1]
+            $givenNameFirstLetter= $givenName.Substring(0,1).ToUpper()
+            $givenNameLastLetters= $givenName.Substring(1)
+            $givenName= $givenNameFirstLetter + $givenNameLastLetters
+            $surname= $user.split(" ")[0].ToUpper()
+            $name= $givenName + " " + $surname
+            $samAccountName= $givenName.ToLower() + "." + $surname.ToLower()
+            $domain= [string] $(Get-ADDomainController |Select-Object Domain)
+            $userPrincipalName= $samAccountName + $domain.split("{")[0] + $domain.split("=")[1].split("}")[0]
+            Write-Output $userPrincipalName
+            #New-ADUser -Name $user -GivenName "Jean" -Surname "DUPONT" -SamAccountName "jdupont" -UserPrincipalName "jdupont@lab.intra" -AccountPassword (ConvertTo-SecureString -AsPlainText "Pàssw0rd" -Force) -Enable $true -ChangePasswordAtLogon $true -Path "OU=Users,OU=IT,OU=Services,DC=LAB,DC=INTRA"
+        }
+        #Write-Output $user.split(" ")[1]
+        #New-ADUser -Name $USER -GivenName "Jean" -Surname "DUPONT" -SamAccountName "jdupont" -UserPrincipalName "jdupont@lab.intra" -AccountPassword (ConvertTo-SecureString -AsPlainText "Pàssw0rd" -Force) -Enable $true -ChangePasswordAtLogon $true -Path "OU=Users,OU=IT,OU=Services,DC=LAB,DC=INTRA"
+    }
+    #Write-Output $flag >> userList.txt
 }
 
 
