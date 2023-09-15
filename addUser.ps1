f2209f5720369d1c5778b4965f06cca0b235767c
Erin Cathignol <erin.cathignol@recia.fr>
2023-09-15 10:37:29 +0200

    pour test sur AD

--- addUser.ps1
+++ addUser.ps1
@@ -12,6 +12,7 @@ $samAccountName= "none"
 $domain= "none"
 $userPrincipalName= "none"
 $accountPassword= "none"
+$dn= "none"
 
 ######### SCRIPT #########
 
@@ -24,18 +25,21 @@ foreach ($user in $data)
         $flag="eleve"
     }
     else {
+        $givenName= $user.split(" ")[1]
+        $givenNameFirstLetter= $givenName.Substring(0,1).ToUpper()
+        $givenNameLastLetters= $givenName.Substring(1)
+        $givenName= $givenNameFirstLetter + $givenNameLastLetters
+        $surname= $user.split(" ")[0].ToUpper()
+        $name= $givenName + " " + $surname
+        $samAccountName= $givenName.ToLower() + "." + $surname.ToLower()
+        $domain= [string] $(Get-ADDomainController |Select-Object Domain)
+        #$domain= "@{Domain=eorxeci.lan}"
+        $userPrincipalName= $samAccountName + $domain.split("{")[0] + $domain.split("=")[1].split("}")[0]
+        $accountPassword= $(-join ("ABCDabcd&@#$%1234".tochararray() | Get-Random -Count 8 | ForEach-Object {[char]$_}))
         if ($flag -eq "personnel") {
-            $givenName= $user.split(" ")[1]
-            $givenNameFirstLetter= $givenName.Substring(0,1).ToUpper()
-            $givenNameLastLetters= $givenName.Substring(1)
-            $givenName= $givenNameFirstLetter + $givenNameLastLetters
-            $surname= $user.split(" ")[0].ToUpper()
-            $name= $givenName + " " + $surname
-            $samAccountName= $givenName.ToLower() + "." + $surname.ToLower()
-            $domain= [string] $(Get-ADDomainController |Select-Object Domain)
-            $userPrincipalName= $samAccountName + $domain.split("{")[0] + $domain.split("=")[1].split("}")[0]
-            Write-Output $userPrincipalName
-            #New-ADUser -Name $user -GivenName "Jean" -Surname "DUPONT" -SamAccountName "jdupont" -UserPrincipalName "jdupont@lab.intra" -AccountPassword (ConvertTo-SecureString -AsPlainText "Pàssw0rd" -Force) -Enable $true -ChangePasswordAtLogon $true -Path "OU=Users,OU=IT,OU=Services,DC=LAB,DC=INTRA"
+            $dn= "OU=Users,OU=Professeurs" + ",DC=" + $domain.split("=")[1].split("}")[0].split(".")[0] + ",DC=" + $domain.split("=")[1].split("}")[0].split(".")[1]
+            #Write-Output "-Name '$name' -GivenName '$givenName' -Surname '$surname' -SamAccountName '$samAccountName' -UserPrincipalName '$userPrincipalName' -AccountPassword (ConvertTo-SecureString -AsPlainText '$accountPassword' -Force) -Enable $true -ChangePasswordAtLogon $true -Path '$dn'"
+            New-ADUser -Name '$name' -GivenName '$givenName' -Surname '$surname' -SamAccountName '$samAccountName' -UserPrincipalName '$userPrincipalName' -AccountPassword (ConvertTo-SecureString -AsPlainText '$accountPassword' -Force) -Enable $true -ChangePasswordAtLogon $true -Path '$dn'
         }
         #Write-Output $user.split(" ")[1]
         #New-ADUser -Name $USER -GivenName "Jean" -Surname "DUPONT" -SamAccountName "jdupont" -UserPrincipalName "jdupont@lab.intra" -AccountPassword (ConvertTo-SecureString -AsPlainText "Pàssw0rd" -Force) -Enable $true -ChangePasswordAtLogon $true -Path "OU=Users,OU=IT,OU=Services,DC=LAB,DC=INTRA"
